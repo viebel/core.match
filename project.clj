e122b7813c4fa1e6441c908bb2a71ba533b3ce90
Yehonathan Sharvit <viebel@gmail.com>
2016-10-19 09:51:50 +0300

    start to port to self-host

--- project.clj
+++ project.clj
@@ -1,21 +1,26 @@
 (defproject org.clojure/core.match "0.3.0-alpha4-SNAPSHOT"
   :description "Optimized pattern matching for Clojure"
 
-  :jvm-opts ^:replace ["-Xmx512m" "-server"]
-
   :test-paths ["src/test/clojure"]
   :source-paths ["src/main/clojure"]
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
                  [org.clojure/tools.analyzer.jvm "0.6.5"]
-                 [org.clojure/clojurescript "0.0-2496" :scope "provided"]]
+                 [org.clojure/clojurescript "1.9.229"]]
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
+  :plugins [[lein-cljsbuild "1.1.1"]
             [cider/cider-nrepl "0.8.1"]]
 
   :cljsbuild
   {:builds
-   [{:id "test"
+   [{:id "dev"
+     :source-paths ["src/main/clojure"]
+     :compiler {:output-to "out/dev/main.dev.js"
+                :output-dir "out/dev"
+                :verbose true
+                :optimizations :simple}
+    }
+    {:id "test"
      :source-paths ["src/test/cljs"]
      :compiler {:output-to "out/test.js"
                 :static-fns true
